612abb5f200b3743ef7e6fd6227bac2338dac8c6
Will <Wi_llo@hotmail.com>
2019-12-11 10:11:59 +1100

    completed initial functioanlity

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,8 @@
 const chatList = document.querySelector('.chat-list');
 const newChatForm = document.querySelector('.new-chat');
 const newNameForm = document.querySelector('.new-name');
+const updateMssg = document.querySelector('.update-mssg');
+const rooms = document.querySelector('.chat-rooms');
 
 // Add a new chat
 newChatForm.addEventListener('submit', (e) => {
@@ -19,11 +21,29 @@ newChatForm.addEventListener('submit', (e) => {
 // Update username
 newNameForm.addEventListener('submit', (e) => {
     e.preventDefault();
+    // Update name via the chatroom class
+    const newName = e.target.name.value.trim();
+    chatroom.updateName(newName);
+    newNameForm.reset();
+    updateMssg.innerText = `Your name was updated to ${newName}`;
+    setTimeout(() => updateMssg.innerText = '', 3000);
 });
+
+// Update the chat room
+rooms.addEventListener('click', (e) => {
+    if(e.target.tagName === 'BUTTON'){
+        chatUI.clear();
+        chatroom.updateRoom(e.target.getAttribute('id'));
+        chatroom.getChats(data => chatUI.render(data));
+    }
+});
+
+// Check local storage for username
+const username = localStorage.username ? localStorage.username : 'anon';
 
 // Class instances
 const chatUI = new ChatUI(chatList);
-const chatroom = new Chatroom('general', 'anon');
+const chatroom = new Chatroom('general', username);
 
 // Get the chats and render
 chatroom.getChats(data => chatUI.render(data));
